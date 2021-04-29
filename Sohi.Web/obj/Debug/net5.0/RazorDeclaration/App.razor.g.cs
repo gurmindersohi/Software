86316ca27ac4bdc2b84b9bd0120d1ddbd7b6799a
gurmindersingh751 <gurminder290195@gmail.com>
2021-04-29 15:18:01 -0400

    Added Identity

--- Sohi.Web/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ Sohi.Web/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -13,70 +13,70 @@ namespace Sohi.Web
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 1 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 2 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 3 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 4 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 5 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 6 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 7 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 8 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 9 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Sohi.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/gurmindersingh/Projects/Software/Sohi/Sohi.Web/_Imports.razor"
+#line 10 "C:\Users\gurmi\Source\Repos\Software\Sohi.Web\_Imports.razor"
 using Sohi.Web.Shared;
 
 #line default
