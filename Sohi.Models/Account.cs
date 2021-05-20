b0321b9675725c70c26967e9edba0239c2506b75
Gurminder Singh <gurmindersingh@Gurminders-iMac.local>
2021-05-19 22:34:42 -0400

    Added business setting tab and created api's to update the business info in database

---
+++ Sohi.Models/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sohi.Models
+{
+    public class Account : ModelBase
+    {
+		public Guid AccountId { get; set; }
+
+		public string AccountName { get; set; }
+		public string AccountType { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string Province { get; set; }
+		public string Country { get; set; }
+		public string PostalCode { get; set; }
+		public string UsersLimit { get; set; }
+
+		public string Logo { get; set; }
+		public DateTime TrialExpiry { get; set; }
+		public Boolean IsAccountPaid { get; set; }
+		public Boolean IsDeleted { get; set; }
+		public Boolean OnHold { get; set; }
+
+		public DateTime HoldDate { get; set; }
+		
+	}
+}
---
+++ Sohi.Web/Pages/Portal/Settings/GeneralBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Sohi.Models;
+using Sohi.Web.Models;
+using Sohi.Web.Services.Settings;
+
+namespace Sohi.Web.Pages.Portal.Settings
+{
+    public class GeneralBase : ComponentBase
+    {
+
+        [Inject]
+        public ISettingsService SettingsService { get; set; }
+
+        public Account Account { get; set; } = new Account();
+
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        [Inject]
+        public UserManager<User> userManager { get; set; }
+
+        public User user { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            var authState = await authenticationStateTask;
+
+            if (authState.User.Identity.IsAuthenticated)
+            {
+                user = await userManager.GetUserAsync(authState.User);
+            }
+          
+                Guid accountid = Guid.Parse(user.AccountId);
+
+                Account = await SettingsService.GetAccount(accountid);
+            
+
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            Account result = null;
+
+            if (Account.AccountId != Guid.Empty)
+            {
+                result = await SettingsService.UpdateAccount(Account);
+            }
+
+            if (result != null)
+            {
+                NavigationManager.NavigateTo("/Portal/Settings/General");
+            }
+
+        }
+    }
+}
