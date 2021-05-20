55f3f7a4365c639b05a0a4ca589be32ecb3ac88f
Gurminder Singh <gurmindersingh@Gurminders-iMac.local>
2021-05-19 23:22:02 -0400

    Create new Page for Business Info

--- Sohi.Models/Account.cs
+++ Sohi.Models/Account.cs
@@ -10,13 +10,19 @@ namespace Sohi.Models
 
 		public string AccountName { get; set; }
 		public string AccountType { get; set; }
+
+		[EmailAddress]
 		public string Email { get; set; }
-		public string Phone { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
 		public string Address { get; set; }
 		public string City { get; set; }
 		public string Province { get; set; }
 		public string Country { get; set; }
-		public string PostalCode { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
 		public string UsersLimit { get; set; }
 
 		public string Logo { get; set; }
