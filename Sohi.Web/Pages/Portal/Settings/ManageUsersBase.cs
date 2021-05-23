77034d7b5f33fbfadff4391c3a71730d88d7972a
Gurminder singh <gurminder290195@gmail.com>
2021-05-23 08:30:00 -0400

    Added Manage user page

---
+++ Sohi.Web/Pages/Portal/Settings/ManageUsersBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Sohi.Web.Pages.Portal.Settings
+{
+    public class ManageUsersBase : ComponentBase
+    {
+        public ManageUsersBase()
+        {
+        }
+    }
+}
