ceef5d6f95379e1f395e19c172dfdf4415dd3c4c
Gurminder Singh <gurmindersingh@Gurminders-iMac.local>
2021-05-26 13:13:09 -0400

    Added Adset page and designing the page

---
+++ Sohi.Models/Adset.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sohi.Models
+{
+    public class Adset
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string OptimizationGoal { get; set; }
+
+        [Required]
+        public string BillingEvent { get; set; }
+
+        [Required]
+        public string BidAmount { get; set; }
+
+        [Required]
+        public string DailyBudget { get; set; }
+
+        [Required]
+        public string CampaignId { get; set; }
+
+        [Required]
+        public string Targeting { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [Timestamp]
+        public DateTime StartTime { get; set; }
+
+
+
+    }
+}
--- Sohi.Web/Pages/Portal/Ads/Facebook/FacebookComponents/NewAdsetBase.cs
+++ Sohi.Web/Pages/Portal/Ads/Facebook/FacebookComponents/NewAdsetBase.cs
@@ -14,6 +14,32 @@ namespace Sohi.Web.Pages.Portal.Ads.Facebook.FacebookComponents
         [Parameter]
         public string CampaignId { get; set; }
 
+        public Adset Adset { get; set; } = new Adset();
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected async Task HandleValidSubmit()
+        {
+            //string endPoint = _config.GetSection("FacebookApp").GetSection("EndPoint").Value;
+            //var content = new FormUrlEncodedContent(new[]
+            //               {
+            //                    new KeyValuePair<string, string>("name", Campaign.Name),
+            //                    new KeyValuePair<string, string>("objective", Campaign.Objective),
+            //                    new KeyValuePair<string, string>("status", "PAUSED"),
+            //                    new KeyValuePair<string, string>("special_ad_categories", "NONE"),
+            //                    new KeyValuePair<string, string>("access_token", Profile.Token)
+            //                });
+
+            //CampaignId = await AdAccountService.CreateFacebookCampaign(AccountId, endPoint, content);
+
+
+            //if (CampaignId != null)
+            //{
+            //    NavigationManager.NavigateTo("/Portal/Ads/Facebook/" + AccountId + "/Create/Adsets/" + CampaignId);
+            //}
+
+        }
 
     }
 }
