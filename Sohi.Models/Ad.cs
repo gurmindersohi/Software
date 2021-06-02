e8721ca1da621bb6a7c8728376dbb9b083dcbbb5
Gurminder Singh <gurmindersingh@Gurminders-iMac.local>
2021-06-02 10:57:25 -0400

    Design the Create New Ad page

--- Sohi.Models/Ad.cs
+++ Sohi.Models/Ad.cs
@@ -14,5 +14,15 @@ namespace Sohi.Models
         public string PrimaryText { get; set; }
 
         public string Destination { get; set; } = "Website";
+
+        public string Headline { get; set; }
+
+        public string WebsitrUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string DisplayLink { get; set; }
+
+        public string CallToAction { get; set; } = "LEARN_MORE";
     }
 }
