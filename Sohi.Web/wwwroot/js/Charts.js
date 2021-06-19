9b0848d9a1622aef7d904d5fc19ccd1ee87b07a0
Gurminder Singh <gurmindersingh@Gurminders-iMac.local>
2021-06-19 10:50:37 -0400

    Added Line Graph for insights

--- Sohi.Web/wwwroot/js/Charts.js
+++ Sohi.Web/wwwroot/js/Charts.js
@@ -1,16 +1,16 @@
 ﻿
 
 
-
 function DrawChart() {
     var ctx = document.getElementById('myChart').getContext('2d');
+
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            labels: [],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: '',
+                data: [],
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
                 borderWidth: 1
@@ -26,3 +26,66 @@ function DrawChart() {
     });
 
 }
+
+
+
+function AddData(datatodisplay, labelstodisplay) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+
+    ctx.myChart.data.labels = labelstodisplay;
+    ctx.myChart.data.datasets[0].data[2] = datatodisplay;
+
+    ctx.myChart.update();
+
+    //var myChart = new Chart(ctx, {
+    //    type: 'line',
+    //    data: {
+    //        labels: labelstodisplay,
+    //        datasets: [{
+    //            label: 'Number of Views',
+    //            data: datatodisplay,
+    //            backgroundColor: 'rgb(255, 99, 132)',
+    //            borderColor: 'rgb(255, 99, 132)',
+    //            borderWidth: 1
+    //        }]
+    //    },
+    //    options: {
+    //        scales: {
+    //            y: {
+    //                beginAtZero: true
+    //            }
+    //        }
+    //    }
+    //});
+
+}
+
+
+
+//function DrawChart(datatodisplay, labelstodisplay) {
+//    var ctx = document.getElementById('myChart').getContext('2d');
+
+//    var myChart = new Chart(ctx, {
+//        type: 'line',
+//        data: {
+//            labels: labelstodisplay,
+//            datasets: [{
+//                label: 'Number of Views',
+//                data: datatodisplay,
+//                backgroundColor: 'rgb(255, 99, 132)',
+//                borderColor: 'rgb(255, 99, 132)',
+//                borderWidth: 1
+//            }]
+//        },
+//        options: {
+//            scales: {
+//                y: {
+//                    beginAtZero: true
+//                }
+//            }
+//        }
+//    });
+
+//}
+  
