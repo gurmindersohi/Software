d92d92dc44883e3d8d429b903704d5b1067f7ba3
Gurminder Sohi <gurmindersohi@Gurminders-MacBook-Pro.local>
2025-07-19 23:57:25 -0400

    remove bin and cache

--- Sohi.Models/obj/Debug/net8.0/Sohi.Models.AssemblyInfo.cs
+++ Sohi.Models/obj/Debug/net8.0/Sohi.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sohi.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7be760687d7ae193922833842f4027096cc32c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbf598ce776d5f8f5b37bf3f5b8a62183ed60dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sohi.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sohi.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sohi.Web/obj/Debug/net8.0/Sohi.Web.AssemblyInfo.cs
+++ Sohi.Web/obj/Debug/net8.0/Sohi.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sohi.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7be760687d7ae193922833842f4027096cc32c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbf598ce776d5f8f5b37bf3f5b8a62183ed60dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sohi.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sohi.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
