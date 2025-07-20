54aacae2f3ceb12df32f94533bbe993cc565d33a
Gurminder Sohi <gurmindersohi@Gurminders-MacBook-Pro.local>
2025-07-20 19:52:26 -0400

    updated migrations

---
+++ Sohi.Api/Migrations/20250720234627_FixDecimalPrecision.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sohi.Api.Migrations
+{
+    public partial class FixDecimalPrecision : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Sohi.Api/Models/AppDbContext.cs
+++ Sohi.Api/Models/AppDbContext.cs
@@ -16,11 +16,18 @@ namespace Sohi.Api.Models
         public DbSet<AdAccount> AdAccounts { get; set; }
 
         public DbSet<Plan> Plans { get; set; }
+        public DbSet<Post> Posts { get; set; } // Add this if Post is not already included
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Plan>(entity =>
+            {
+                entity.Property(p => p.Price).HasPrecision(18, 2); // Example: 18 digits, 2 decimal places
+                entity.Property(p => p.Tax).HasPrecision(18, 2);
+                entity.Property(p => p.Total).HasPrecision(18, 2);
+            });
         }
     }
 }
